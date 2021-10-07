a7025e8996e1531f480311946d49154794d9c571
Pheladi-M <91885044+Pheladi-M@users.noreply.github.com>
2021-10-07 12:46:09 +0200

    Add files via upload

---
+++ coding_course_/js/One.js
@@ -0,0 +1,9 @@
+let btn = document.createElement ("button");
+btn.innerHTML = "Click Me";
+btn.type = "click";
+btn.name = "One";
+document.body.appendChild(btn);
+btn.onclick = function () {
+	alert("Have a good day :)");
+};
+document.bodyappendChild(btn);
